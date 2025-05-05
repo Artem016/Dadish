7b947c19a288d1ea01cc96abd4c8ed0307fd68d5
Артем Клейменов <lenovaega2@gmail.com>
2025-05-05 19:10:00 +0500

    UI выбора уровня 00

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
-    [SerializeField] private GameObject _blockedVisual;
+    [SerializeField] private GameObject _blockedVisual, _unBlockVisual;
+    [SerializeField] private Text _text;
     [SerializeField] private Button _button;
     [SerializeField] private int _levelNumber;
     [SerializeField] private bool _isBlocked = true;
+    [SerializeField] private Color _blockColorText, _unBlockColorText;
 
     private void Start()
     {
@@ -21,11 +23,15 @@ public class LevelButton : MonoBehaviour
         if (_isBlocked)
         {
             _blockedVisual.SetActive(true);
+            _unBlockVisual.SetActive(false);
+            _text.color = _blockColorText;
             _button.interactable = false;
         }
         else
         {
             _blockedVisual.SetActive(false);
+            _unBlockVisual.SetActive(true);
+            _text.color = _unBlockColorText;
             _button.interactable = true;
         }
     }
